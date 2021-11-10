faf4817c4d4249f0bb75ea3e6d24a0febfff41b0
Ioannis Canellos <iocanel@gmail.com>
2021-11-10 08:13:42 +0200

    feat: add filteropts function

--- library.sh
+++ library.sh
@@ -37,3 +37,53 @@ function or() {
     echo $2
   fi
 }
+
+#
+# Return the nth first arguments
+function nfirstargs() {
+        num=$1
+        current=0
+        shift
+        for var in "$@";do
+                if [ $current -lt $num ]; then
+                        current=$(($current + 1))
+                        echo $var
+                else
+                        break
+                fi
+        done
+} 
+
+#
+# Filter the specified number of options and flags
+# Example:
+#   filteropts 2 -version --dry-run foo bar baz
+#   -> foo bar baz
+function filteropts() {
+        num=$1
+        if echo $num | grep '[^0-9]' > /dev/null; then
+                echo "$@"
+        else
+                skip=$(($num+2))
+                tofilter=`nfirstargs $@`
+                next=false
+                #Loop through arguments starting from skip
+                for candidate in "${@:$skip}"; do
+                        if $next; then
+                                next=false
+                                continue;
+                        fi
+
+                        #Loop through items that need to be skiped and see if candidate is included
+                        for i in $tofilter; do
+                                if [ $candidate = "$i" ]; then
+                                        next=true
+                                        break;
+                                fi
+                        done
+                        if ! $next; then
+                                echo $candidate
+                        fi
+                done
+        fi
+}
